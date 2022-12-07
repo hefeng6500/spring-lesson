d5522e2ab56087ba39fbffac2a50e2a95a30cefe
hefeng6500 <2443992009@qq.com>
2022-12-07 20:51:51 +0800

    feat: 第三方 bean 加载 properties 文件,读取单个属性

---
+++ spring_03_third_party_properties/src/main/java/com/yang/dao/BookDaoImpl.java
@@ -0,0 +1,13 @@
+package com.yang.dao;
+
+public class BookDaoImpl implements BookDao {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void save() {
+        System.out.println("book dao save ..." + name);
+    }
+}
