13659501cd1d5448ddc1452ea30b855da368c7aa
hefeng6500 <2443992009@qq.com>
2022-12-19 22:00:35 +0800

    feat: 接口、方法签名获取

--- aop_my_04/src/main/java/com/yang/config/ProjectAdvice.java
+++ aop_my_04/src/main/java/com/yang/config/ProjectAdvice.java
@@ -1,6 +1,7 @@
 package com.yang.config;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -15,13 +16,21 @@ public class ProjectAdvice {
 
     @Around("servicePt()")
     public void runSpeed(ProceedingJoinPoint pjp) throws Throwable {
+        //获取执行签名信息
+        Signature signature = pjp.getSignature();
+
+        //通过签名获取执行操作名称(接口名)
+        String className = signature.getDeclaringTypeName();
+
+        //通过签名获取执行操作名称(方法名)
+        String methodName = signature.getName();
+
         long start = System.currentTimeMillis();
 
         for (int i = 0; i < 10000; i++) {
              pjp.proceed();
         }
         long end = System.currentTimeMillis();
-        System.out.println("业务层接口万次执行时间: "+(end-start)+"ms");
-
+        System.out.println("万次执行："+ className+"."+methodName+"---->" +(end-start) + "ms");
     }
 }
