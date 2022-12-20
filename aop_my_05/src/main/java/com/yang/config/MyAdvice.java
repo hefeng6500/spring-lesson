3cfff6562c46100cc139f22b475fcf58ffc56e67
hefeng6500 <2443992009@qq.com>
2022-12-20 21:01:18 +0800

    feat: 环绕通知获取参数和修改原始参数

--- aop_my_05/src/main/java/com/yang/config/MyAdvice.java
+++ aop_my_05/src/main/java/com/yang/config/MyAdvice.java
@@ -16,7 +16,7 @@ public class MyAdvice {
     @Before("pt()")
     public void before(JoinPoint jp) {
         Object[] args = jp.getArgs();
-        System.out.println("before 切入点获取参数：" + Arrays.toString(args));
+        System.out.println("前置通知获取参数：" + Arrays.toString(args));
         System.out.println("before advice ..." );
     }
 
@@ -27,7 +27,15 @@ public class MyAdvice {
 
     @Around("pt()")
     public Object around(ProceedingJoinPoint pjp) throws Throwable{
-        Object ret = pjp.proceed();
+        Object[] args = pjp.getArgs();
+        System.out.println("环绕通知获取参数：" + Arrays.toString(args));
+
+//        调用无参数的proceed，当原始方法有参数，会在调用的过程中自动传入参数
+//        所以调用这两个方法的任意一个都可以完成功能
+//        但是当需要修改原始方法的参数时，就只能采用带有参数的方法
+//        有了这个特性后，我们就可以在环绕通知中对原始方法的参数进行拦截过滤，避免由于参数的问题导致程序无法正确运行，保证代码的健壮性。
+        args[0] = 222;
+        Object ret = pjp.proceed(args);
         return ret;
     }
     @AfterReturning("pt()")
