1e23f5ebc747ba83b5a91bb842b16182e1494ab6
hefeng6500 <2443992009@qq.com>
2022-12-21 20:22:01 +0800

    feat: spring 事务处理

--- aop_my_06/src/main/java/com/yang/service/AccountServiceImpl.java
+++ aop_my_06/src/main/java/com/yang/service/AccountServiceImpl.java
@@ -3,14 +3,17 @@ package com.yang.service;
 import com.yang.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountDao accountDao;
 
+    @Transactional
     public void transfer(String out, String in, Double money) {
         accountDao.outMoney(out,money);
+        int i = 1/0;
         accountDao.inMoney(in,money);
     }
 }
